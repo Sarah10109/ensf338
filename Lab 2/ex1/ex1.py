49d5f41151732a7752d6e2f3331faff56a796f48
Sarah10109 <62390879+Sarah10109@users.noreply.github.com>
2024-02-02 14:04:08 -0700

    Create delete.txt

---
+++ Lab 2/ex1/ex1.py
@@ -0,0 +1,16 @@
+'''
+The code gets an integer n and check if n is equal to 1 or 0. 
+If not then it the functions calls it self as a recursion function. 
+This code is finding the fibonacci sequence to the nths place.  
+'''
+
+'''
+This example is a bad example of divide-and-conquer algorithm since 
+it is creating a new problem without making the problem smaller.
+The function does split up the work function into smaller parts which 
+will go to smaller parts untill n reaches 1 or 0.
+'''
+
+'''
+The time complexity of the algorithm is O(2^n)
+'''
