625dd9a9fddb757a69fd5a5d9c1801b9b3b975c2
Suprova <suprovahoque@gmail.com>
2024-02-14 15:03:57 -0700

    File created for exercise 7

---
+++ Lab 3/ex5.py
@@ -0,0 +1,14 @@
+# 1. Implement a standard binary search, with the following tweak: the
+# midpoint for the first iteration must be configurable (all successive
+# iterations will just split the array in the middle)
+
+
+# 2. Time the performance of each search task w/ different midpoints
+# for each task. You can use whatever strategy you want to check
+# different midpoints. Then, choose the best midpoint for each task 
+
+# 3. Produce a scatterplot visualizing each task and the corresponding
+# chosen midpoint
+
+# 4. Comment on the graph. Does the choice of initial midpoint appear
+# to affect performance? Why do you think is that?
