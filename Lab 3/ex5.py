d452d2c9e136ff71dcaf659a6c196258ebf2bded
Suprova <suprovahoque@gmail.com>
2024-02-14 17:49:25 -0700

    Modified binary search function created

--- Lab 3/ex5.py
+++ Lab 3/ex5.py
@@ -2,10 +2,27 @@
 # midpoint for the first iteration must be configurable (all successive
 # iterations will just split the array in the middle)
 
+def binary_search(array, first, last, num, initial_midpoint=None):
+    while first <= last:
+        if initial_midpoint is not None:
+            midpoint = initial_midpoint
+            initial_midpoint = None  # reset to None to ensure the initial midpoint is used only for the first iteration
+        else:
+            midpoint = (first + last) // 2
+        if num == array[midpoint]:
+            return midpoint  # key at which num is located
+        elif num < array[midpoint]:
+            last = midpoint - 1
+        else:
+            first = midpoint + 1
+    return -1  # num not found in array
+
 
 # 2. Time the performance of each search task w/ different midpoints
 # for each task. You can use whatever strategy you want to check
 # different midpoints. Then, choose the best midpoint for each task 
+
+
 
 # 3. Produce a scatterplot visualizing each task and the corresponding
 # chosen midpoint
