613bfdccefaff3b1835a73e1fc4882f6e8827b44
Suprova <suprovahoque@gmail.com>
2024-03-20 00:37:32 -0600

    Finished

--- Lab 7/ex2.py
+++ Lab 7/ex2.py
@@ -24,3 +24,127 @@ appropriate sequence of nodes: [0.2 pts]
     supported”)
 
 '''
+
+
+# Extending the BST from exercise 1 into an AVL tree
+
+class Node:
+    def __init__(self, data, parent=None, left=None, right=None):
+        self.parent = parent
+        self.data = data
+        self.left = left
+        self.right = right
+        self.balance = 0 # balance attribute
+
+def insert(data, root=None):
+    current = root
+    parent = None
+
+    while current is not None:
+        parent = current
+        if data <= current.data:
+            current = current.left
+        else:
+            current = current.right
+
+    newnode = Node(data, parent)    
+    if root is None:
+        root = newnode
+    elif data <= parent.data:
+        parent.left = newnode
+    else:
+        parent.right = newnode
+    
+    # Update the balance of the nodes and determine the pivot node
+    pivot = None
+    current = newnode
+    while current is not None:
+        current.balance = calcbalance(current)
+        if abs(current.balance) > 1:
+            pivot = current
+        current = current.parent
+   
+    # Checking Case #1: Pivot not detected
+    if pivot is None:
+        print("Case #1: Pivot not detected")
+    
+    # Checking Case #2: Pivot exists but node is inserted into shorter subtree
+    elif (pivot.balance > 0 and newnode.data < pivot.data) or (pivot.balance < 0 and newnode.data > pivot.data):
+        print("Case #2: A pivot exists, and a node was added to the shorter subtree")
+    else:
+        print("Case #3: Not supported") # if none of the above, case 3 must be true, hence the message "Case #3: Not supported" is printed
+
+    return newnode
+
+
+def search(data, root):
+    current = root
+    while current is not None:
+        if data == current.data:
+            return current
+        elif data <= current.data:
+            current = current.left
+        else:
+            current = current.right
+    return None
+
+def calcbalancetree(root, biggest): 
+    if root is not None:
+        data = (abs(calcbalance(root))) # Depth-first, pre-order traversal style
+        if data > biggest:
+            biggest = data
+        check2 = calcbalancetree(root.left, biggest)
+        check1 = calcbalancetree(root.right, biggest)
+        if check1 > biggest:
+            biggest = check1
+        if check2 > biggest:
+            biggest = check2
+    return biggest
+
+def height(node):
+    if node is None:
+        return 0
+    return 1 + max(height(node.left), height(node.right)) #returns the side that keeps going incase one side is None and the other continues
+
+def calcbalance(root):
+    if root is None:
+        return 0
+    return height(root.right) - height(root.left)
+
+# Checks for Case #3
+def check_case_3(root):
+    if root is not None:
+        if (root.balance > 0 and root.left and root.left.balance > 0) or (root.balance < 0 and root.right and root.right.balance < 0):
+            print("Case #3: Not supported")
+        check_case_3(root.left)
+        check_case_3(root.right)
+
+def leftRotate(node):
+    rightChild = node.right
+    leftChild = rightChild.left
+    rightChild.left = node
+    node.right = leftChild
+    node.balance = calcbalance(node)
+    rightChild.balance = calcbalance(rightChild)
+    return rightChild
+
+def rightRotate(node):
+    leftChild = node.left
+    rightChild = leftChild.right
+    leftChild.right = node
+    node.left = rightChild
+    node.balance = calcbalance(node)
+    leftChild.balance = calcbalance(leftChild)
+    return leftChild
+
+
+
+# Test cases
+root = None
+root = insert(30, root) # case 1
+root = insert(20, root) # case 1
+root = insert(40, root) # case 2
+root = insert(10, root) # case 3
+
+
+
